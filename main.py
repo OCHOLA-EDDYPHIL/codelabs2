5e067a3d0d35dc8f2545b937ccf2f1fafd02916b
2024ines <ines.meshack@strathmore.edu>
2024-05-22 15:24:07 +0300

    Read excel function file defined

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from time import time
 INPUT_FIlE_PATH = 'input_data_files/'
 OUTPUT_FILE_PATH = 'output_data_files/'
 
-
 if __name__ == '__main__':
     print(time())
 
+
+def main():
+    # Read data
+
+    df = pd.read_excel('Test files.xlsx')
