825f9ff37e7f8b55bac438fa2296ab20b208df4b
leo10ne <leone.chamwada@strathmore.edu>
2024-05-22 15:12:31 +0300

    special character names defined

--- tasks.py
+++ tasks.py
@@ -7,9 +7,13 @@ def generate_email_address():
     pass
 
 
-def generate_special_char_names():
-    pass
+def special_character_names(df):
+    pattern = re.compile(r"[^\w\s]")
+    special_char_names = df[df['Last Name'].apply(lambda x: bool(pattern.search(x)))]
+    special_char_names.to_csv('special_char_names.csv', index=False)
 
+    with open('logs.txt', 'a') as log_file:
+        log_file.write(f"Special character names:\n{special_char_names.to_string(index=False)}\n")
 
 def read_excel_file() -> list:
     pass
