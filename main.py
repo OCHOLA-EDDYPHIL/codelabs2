1e38b5348eb12a92923634dc4b4a5b018ad8208a
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2024-05-22 14:12:32 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+This python module is used to run all the functions in the project
+"""
+import pandas as pd
+import tasks
+from time import time
+
+INPUT_FIlE_PATH = 'input_data_files/'
+OUTPUT_FILE_PATH = 'output_data_files/'
+
+
+if __name__ == '__main__':
+    print(time())
+
---
+++ tasks.py
@@ -0,0 +1,15 @@
+"""
+This modula has all the reusable functions for tasks
+"""
+
+
+def generate_email_address():
+    pass
+
+
+def generate_special_char_names():
+    pass
+
+
+def read_excel_file() -> list:
+    pass
