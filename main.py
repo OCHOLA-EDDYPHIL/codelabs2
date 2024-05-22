f38ebbb39856f10f1349b7962e872fe0f81f4979
CRAlex2024 <alexg.muiruri@strathmore.edu>
2024-05-22 15:32:47 +0300

    Alex

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ OUTPUT_FILE_PATH = 'output_data_files/'
 if __name__ == '__main__':
     print(time())
 
+def main():
+ # Generate email addresses
+    df['Email'] = df.apply(lambda row: generate_email(row['First Name'], row['Last Name']), axis=1)
--- tasks.py
+++ tasks.py
@@ -1,10 +1,14 @@
 """
 This modula has all the reusable functions for tasks
 """
+import re
+import pandas as pd
+import numpy as np
 
 
-def generate_email_address():
-    pass
+def generate_email(first_name, last_name):
+    clean_last_name = re.sub(r'\W+', '', last_name).lower()
+    return f"{first_name[0].lower()}{clean_last_name}@gmail.com"
 
 
 def generate_special_char_names():
